06ce1fe54aee61f707bdbf246b0634b9d51706fe
Ritabrata Pal <132898086+rito3668@users.noreply.github.com>
2025-01-11 01:06:48 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -13,7 +13,7 @@ const AppContextProvider = (props) => {
 
     const [credit, setCredit] = useState(false)
 
-    const backendUrl = "https://imagify-server-5rbv.onrender.com/"
+    const backendUrl = "https://imagify-server-5rbv.onrender.com"
     const navigate = useNavigate()
 
     const loadCreditsData = async () => {
